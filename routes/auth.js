cc5a3643182ebdd531f70c39220c087b9ea6bd95
markand dighe <110598529+markandyuvasoft@users.noreply.github.com>
2022-10-20 05:41:43 -0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -58,7 +58,7 @@ const sendset = async (name, email, token) => {
         from: 'amandighe0@gmail.com',
         to: email,
         subject: 'reset password',
-        html: '<p> hii ' + name + ', plz copy the link and <a href="https://adsasas.herokuapp.com/reset?token=' + token + '"> reset your password</a>'
+        html: '<p> hii ' + name + ', plz copy the link and <a href="https://dasadsdas.herokuapp.com/reset?token=' + token + '"> reset your password</a>'
     };
 
     transporter.sendMail(mailoptions, function (err, info) {
