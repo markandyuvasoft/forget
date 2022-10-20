da3db4cc2f9c394f0ba0bf301aa4a3ddffac11af
markand dighe <110598529+markandyuvasoft@users.noreply.github.com>
2022-10-20 05:49:46 -0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -47,15 +47,15 @@ const sendset = async (name, email, token) => {
         port: 465,                     // true for 465, false for other ports
         host: "smtp.gmail.com",
         auth: {
-            user: 'user1998markand@gmail.com',
-            pass: 'gcnwsttrwxcoptsr'
+            user: '',
+            pass: ''
         },
         secure: true,
     });
 
     const mailoptions = {
 
-        from: 'user1998markand@gmail.com',
+        from: '',
         to: email,
         subject: 'reset password',
         html: '<p> hii ' + name + ', plz copy the link and <a href="https://dasadsdas.herokuapp.com/reset?token=' + token + '"> reset your password</a>'
